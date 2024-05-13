8fd1a48e94ba9af7824808f0181b53f9f0e23bdc
EdEquinox <zemarquez97@hotmail.com>
2024-05-13 21:58:27 +0100

    Update settings.py

--- MusicboardExtract_WEB/settings.py
+++ MusicboardExtract_WEB/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y!eue!+w$!7(!zu(qned@r2j9xx#vl0054914_om)31a9=ewh*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
